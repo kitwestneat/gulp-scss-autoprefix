250df1e963354f67afef5114411765c2c5ae6266
Kit Westneat <kit.westneat@gmail.com>
2016-10-24 17:26:36 -0400

    add default task and pull BASEDIR env variable if it exists. Also add gulp-less to dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var baseDir = process.env.BASEDIR || '../mirror2-ati-theme';
+var styleDir = baseDir + '/assets/stylesheets/'
 
 var gulp = require('gulp'),
 	autoprefixer = require('gulp-autoprefixer'),
@@ -10,26 +12,24 @@ var gulp = require('gulp'),
 	plumber = require('gulp-plumber'),
 	watch = require('gulp-watch'),
 	sourcemaps = require('gulp-sourcemaps'),
-	stylesGlob = ['./../mirror2-ati-theme/assets/stylesheets/**'],
+	stylesGlob = [ styleDir + '**' ],
 
 	onError = function(err) {notify(err)};
 
+var compileStyle = function() {
+	return gulp.src( styleDir + 'main.scss' )
+		.pipe( plumber({errorHandler: notify.onError({'message': 'Error: <%= error.message %>'})}) )
+		.pipe( sass() )
+		.pipe( autoprefixer('last 20 versions', 'ie 8') )
+		.pipe( cleanCSS({compatibility: 'ie8'}) )
+		.pipe( cleanCSS() )
+		.pipe( sourcemaps.write() )
+		.pipe( notify({message: "compiled"}) )
+		.pipe( gulp.dest(baseDir + '/css/') )
+};
 
+gulp.task('default', compileStyle);
 gulp.task('watch', function() {
-
-	watch(stylesGlob, function (event) {
-
-		return gulp.src( './../mirror2-ati-theme/assets/stylesheets/main.scss' )
-			.pipe( plumber({errorHandler: notify.onError({'message': 'Error: <%= error.message %>'})}) )
-			.pipe( sass() )
-			.pipe( autoprefixer('last 20 versions', 'ie 8') )
-			.pipe( cleanCSS({compatibility: 'ie8'}) )
-	        .pipe( cleanCSS() )
-			.pipe( sourcemaps.write() )
-			.pipe( notify({message: "compiled"}) )
-			.pipe( gulp.dest("./../mirror2-ati-theme/css/") )
-	})
- 
+	watch(stylesGlob, compileStyle)
 	.on('error', function() {return true;})
-
 });
